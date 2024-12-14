3852665ffda417b7feaea8432ee93d3ca9689da8
ashraf <ashrafchekintakath@gmail.com>
2024-12-14 20:44:29 +0530

    grappeli

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # "admin_interface",
+    # "colorfield",
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'firstapp',
+    
+    
     
 ]
 
