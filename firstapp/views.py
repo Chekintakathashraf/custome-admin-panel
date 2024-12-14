15056aa9f7c8e71b691666a7808c23f7dd6d7c4b
ashraf <ashrafchekintakath@gmail.com>
2024-12-14 21:50:45 +0530

    kjkk

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def index(request):
+    return render(request,'admin.html')
+
+def create_customer(request):
+    customers = Customer.objects.all()
+    return render(request,'create_customer.html',{'customers':customers})
+
+def customers(request):
+    return render(request,'customers.html')
+
--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from firstapp.views import *
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')), # grappelli URLS
     path('admin/', admin.site.urls),
+    
+    path('',index),
+    path('create-customer/',create_customer),
+    path('customers/',customers),
 ]
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
